1833f5132411f2510531998ff9a8e7df9d61d761
ckckcklab <ckilpatrick82@gmail.com>
2020-11-20 13:12:49 +0000

    first ACG-ETL commit

--- acg_etl/acg_etl_stack.py
+++ acg_etl/acg_etl_stack.py
@@ -1,9 +1,70 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    core,
+    aws_lambda as _lambda,
+    aws_dynamodb,
+    aws_ec2 as ec2,
+    aws_rds as rds,
+    aws_events,
+    aws_events_targets,
+)
 
 class AcgEtlStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Creates VPC
+        vpc = ec2.Vpc(self, id)
+
+        # Creates RDS Database
+        instance = rds.DatabaseInstance(
+            self, "RDS",
+            database_name="covid",
+            engine=rds.DatabaseInstanceEngine.mysql(
+                version=rds.MysqlEngineVersion.VER_8_0_21
+            ),
+            vpc=vpc,
+            port=3306,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2,
+                ec2.InstanceSize.MICRO,
+            ),
+            removal_policy=core.RemovalPolicy.DESTROY,
+            deletion_protection=False,
+            vpc_placement=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+        )
+
+        # Add inbound rule to RDS SG
+        instance.connections.allow_from_any_ipv4(ec2.Port.tcp(3306), "Open to the world")
+
+        # Defines Lambda layers
+        python_etl_layer = _lambda.LayerVersion(
+            self, "python-etl",
+            code=_lambda.AssetCode('C:\Code\ACG Challenge – AWS ETL\python.zip'))
+        
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'etlhandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='etl.extract_csv1',
+            layers=[python_etl_layer],
+            timeout=core.Duration.seconds(30),
+            memory_size=(256)
+        )
+
+        # Add Lambda Environment Variables
+        my_lambda.add_environment("RDS_INSTANCE", instance.instance_endpoint.hostname)
+        my_lambda.add_environment("SECRET_NAME", instance.secret.secret_full_arn)
+        
+        # Grant permission to lambda to access RDS Secret
+        instance.secret.grant_read(my_lambda)
+
+        # Create a Cloudwatch Event rule
+        four_hour_rule = aws_events.Rule(
+            self, "four_hour_rule",
+            schedule=aws_events.Schedule.rate(core.Duration.minutes(240)),
+        )
+
+        # Add target to Cloudwatch Event
+        four_hour_rule.add_target(aws_events_targets.LambdaFunction(my_lambda))
---
+++ lambda/etl.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import boto3
+import os
+from sqlalchemy import create_engine
+import pymysql
+import json
+
+def extract_csv1(event, context):
+#Extract 1st CSV
+
+  cols = ["Date","Country/Region", "Recovered"]
+
+  url1 = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv"
+
+  rec = pd.read_csv(url1, usecols=cols)
+
+  return rec
+
+def extract_csv2(event):
+#Extract 2nd CSV
+
+  url2 = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
+
+  data = pd.read_csv(url2)
+
+  return data
+
+def transform_csv(data, rec):
+#Transform CSVs
+
+  data = pd.DataFrame(data)   
+
+  data = data.drop([0])
+
+  data.rename(columns={'date': 'Date', 'cases': 'Cases', 'deaths': 'Deaths'}, inplace=True) 
+
+  usrec = rec[rec["Country/Region"] == "US"]  
+
+  usrec = usrec.drop(columns=['Country/Region'])
+
+  data = data.merge(usrec, on='Date')
+
+  return data
+
+#Call Extract and Transform Functions
+
+rec = extract_csv1({
+  "key1": "value1"},1)
+
+data = extract_csv2({
+  "key1": "value1"})
+
+data = transform_csv(data, rec)
+
+def load_to_db(data):
+#Retrieve AWS Secret
+
+  secret_name = os.environ['SECRET_NAME']
+  region_name = "eu-west-2"
+  session = boto3.session.Session()
+  client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name)
+
+  secret_value = client.get_secret_value(
+    SecretId=secret_name)
+
+  dbpass = secret_value['SecretString']
+  j = json.loads(dbpass)
+  dbpass = j['password']
+
+  rds_instance = os.environ['RDS_INSTANCE']
+  
+  db_con = 'mysql+pymysql://admin:%s@%s/covid' % (dbpass, rds_instance)
+
+  sqlEngine = create_engine(db_con, pool_recycle=3600)
+
+  dbConnection = sqlEngine.connect()
+
+  tableName = 'coviddata'
+
+  data.to_sql(tableName, dbConnection, if_exists='replace')
+  
+  return
+
+data = load_to_db(data)
